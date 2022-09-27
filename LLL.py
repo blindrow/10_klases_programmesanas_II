33264c441f21de2036350a8f3d7090795b9e45c6
blindrow <114405600+blindrow@users.noreply.github.com>
2022-09-27 09:58:40 +0300

    Add files via upload

---
+++ LLL.py
@@ -0,0 +1,30 @@
+print("Cik rezisoru ķedē?")
+count=int(input())
+if count ==2:
+    
+
+    r1=int(input("R1= "))
+
+    r2=int(input("R2= "))
+
+
+    r=round(1/(1/r1+1/r2),2)
+
+    print=(r)
+
+if count ==3:
+
+    r1=int(input("R1= "))
+
+    r2=int(input("R2= "))
+
+    r3=int(input("R3= "))
+
+
+    r=round(1/(1/r1+1/r2+1/r3),2)
+
+    print=(r)
+
+
+
+
